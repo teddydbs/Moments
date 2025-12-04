4f30afb81f69dcec10e47beb3f93cc0ab6f45b0f
Ton Nom <ton.email@example.com>
2025-12-04 17:13:04 +0100

    Initial Commit

---
+++ Moments/MomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentsApp.swift
+//  Moments
+//
+//  Created by Teddy Dubois on 04/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct MomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
